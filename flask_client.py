8cad43d7040ed35a536980f725f1517d7830c98b
justcraft0rr <justcraftytgm@gmail.com>
2026-09-08 16:02:36 +0200

    requirements to the inner classes

--- flask_client.py
+++ flask_client.py
@@ -5,3 +5,14 @@ class FlaskClient:
             'flask_host_ip': kwargs.get('flask_host_ip'),
             'flask_host_port': kwargs.get('flask_host_port')
         }
+        self.Twitch = self.twitch(self.requirements)
+        self.Youtube = self.youtube(self.requirements)
+    
+    class twitch:
+        def __init__(self, info):
+            self.requirements = info
+    
+    class youtube:
+        def __init__(self, info):
+            self.requirements = info
+
